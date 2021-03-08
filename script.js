e089c8c6d3f7052118aab7d6f835b6c21d72673e
Alex Federovitch <alexfederovitch@yahoo.com>
2021-03-08 00:15:08 -0600

    Updating files after making app

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("button").onclick = function () {checker()};
+document.getElementById("reset").onclick = function () {reset()};
+
+function checker() {
+    let birthday = new Date(document.getElementById("text").value);
+    let currentDate = new Date();
+    let differenceInTime = currentDate.getTime() - birthday.getTime();
+    let differenceInDays = "Your age in days is: " + Math.floor(differenceInTime/(1000*3600*24));
+    document.getElementById("result").innerHTML = differenceInDays;
+    console.log(differenceInDays);
+}
+
+function reset() {
+    document.getElementById("text").value = "";
+    console.log("hello")
+}
